71a840617d005445bbb1f66fa301467a8c721ad6
Aryan Bhavsar <aryan.bhavsar@gasleaksensors.com>
2026-04-07 19:14:39 +0530

    Registered the router level middleware for id validation

--- NodeJs-Express-MongoDB/Books-API/routes/book.routes.js
+++ NodeJs-Express-MongoDB/Books-API/routes/book.routes.js
@@ -4,14 +4,17 @@ const { validateId, validateBody } = require('../middlewares/validation')
 
 const router = express.Router();
 
+// Router-level Middleware
+router.use('/:id', validateId);
+
 router.get('/', controller.getAllBooks)
 
-router.get('/:id', validateId, controller.getBookById)
+router.get('/:id', controller.getBookById)
 
 router.post('/', validateBody, controller.addBook)
 
-router.delete('/:id', validateId, controller.deleteBook)
+router.delete('/:id', controller.deleteBook)
 
-router.put('/:id', validateId, validateBody, controller.updateBook)
+router.put('/:id', validateBody, controller.updateBook)
 
 module.exports = router;
