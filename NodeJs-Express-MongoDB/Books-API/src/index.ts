f79ba000d165ebbed9b9ea6abd3f8fd9245c0f76
Aryan Bhavsar <aryan.bhavsar@gasleaksensors.com>
2026-04-17 12:39:44 +0530

    Implemented basic rate limiter middleware

--- NodeJs-Express-MongoDB/Books-API/src/index.ts
+++ NodeJs-Express-MongoDB/Books-API/src/index.ts
@@ -4,6 +4,8 @@ import { connectMongoDB } from './connection.js';
 import v1Router from './routes/v1/v1.routes.js'
 import { loggerMiddleware } from './middlewares/logger.js'
 import { errorHandler } from './middlewares/errorHandler.js'
+import rateLimiter from './middlewares/rateLimiter.js';
+
 const PORT = 8000;
 const app = express();
 
@@ -17,6 +19,7 @@ connectMongoDB(mongoURL).then(() => console.log("MongoDB connected"));
 // Middlewares (Plugins)
 app.use(express.json());
 app.use(loggerMiddleware);
+app.use(rateLimiter);
 
 app.use('/api/v1', v1Router);
 
