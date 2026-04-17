7af068876fda08efbb6904ded4467806012f3b69
Aryan Bhavsar <aryan.bhavsar@gasleaksensors.com>
2026-04-17 14:58:30 +0530

    modified options for rate limiter

--- NodeJs-Express-MongoDB/Books-API/src/middlewares/rateLimiter.ts
+++ NodeJs-Express-MongoDB/Books-API/src/middlewares/rateLimiter.ts
@@ -2,7 +2,13 @@ import rateLimit from "express-rate-limit";
 
 const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 10,
+  message: "Rate limit exceeded. You can only make 100 requests every 15 mins.",
+  handler: (req, res, next, options) => {
+    next({message: options.message, statusCode: options.statusCode})
+  },
+  standardHeaders: true,
+  legacyHeaders: false
 });
 
 export default rateLimiter;
