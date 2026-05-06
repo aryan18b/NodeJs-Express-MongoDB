aba321f25a8b035cf8f57f77b964d83b96ca906b
Aryan Bhavsar <aryan.bhavsar@gasleaksensors.com>
2026-05-06 11:21:20 +0530

    Moved the mongo connection URI to the .env file

--- NestJs/nest-blog-api/src/app.module.ts
+++ NestJs/nest-blog-api/src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://aryanbhavsar95_db_user:aryan18083@cluster0.qaegiay.mongodb.net/NestJs',
-    ),
+    ConfigModule.forRoot({isGlobal: true}),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_HOST')
+      })
+    }),
     UsersModule
   ],
 })
